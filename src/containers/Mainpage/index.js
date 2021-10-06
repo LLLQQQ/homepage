2d4677ef2cd2d86e0531533c8d5447afbf519b07
qiaolei <1170494354@qq.com>
2021-10-06 16:26:35 +0800

    add btn content and style

--- src/containers/Mainpage/index.js
+++ src/containers/Mainpage/index.js
@@ -4,7 +4,11 @@ function Page() {
   return (
     <div className={style["wrapper"]}>
       <div className="header">{"Broccoli & Co."}</div>
-      <div className="content"></div>
+      <div className="content">
+        <div className="title">A better way<br />to enjoy every day.</div>
+        <div className="subtitle">Be the first to know when we launch.</div>
+        <div className="btn">Request an invite</div>
+      </div>
       <div className="footer">Made with ❤ in Melbourne.<br />© 2016 {"Broccoli & Co."} All rights reserved.</div>
     </div>
   );
