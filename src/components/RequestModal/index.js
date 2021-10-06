44c99a8047e737548b6ceadd8de5ea43705d583f
qiaolei <1170494354@qq.com>
2021-10-06 17:18:59 +0800

    udpate request modal content and stylr

--- src/components/RequestModal/index.js
+++ src/components/RequestModal/index.js
@@ -1,24 +1,38 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import style from './index.module.scss';
 
 function Comp(props) {
   const { onCancel } = props;
-  const maskOnClick = useCallback(() => {
+  const [requestDone, setRequestDone] = useState(false);
+  const cancelOnClick = useCallback(() => {
     if (typeof onCancel === 'function') {
       onCancel();
     };
   }, [onCancel]);
+  const sendOnClick = useCallback(() => {
+    setRequestDone(true);
+  }, []);
   return (
     <div className={style["wrapper"]}>
-      <div className="mask" onClick={maskOnClick}></div>
-      <div className="content">
-        <div className="title">Request an invite</div>
-        <input className="input" placeholder="Full name" />
-        <input className="input" placeholder="Email" />
-        <input className="input" placeholder="Confirm email" />
-        <div className="btn">Send</div>
-      </div>
-    </div>
+      <div className="mask" onClick={cancelOnClick}></div>
+      {
+        requestDone ? (
+          <div className="content done">
+            <div className="title">All done!</div>
+            <div className="content">You will be one of the first to experience<br />{"Broccoli & Co."} when we launch.</div>
+            <div className="btn" onClick={cancelOnClick}>OK</div>
+          </div>
+        ) : (
+          <div className="content">
+            <div className="title">Request an invite</div>
+            <input className="input" placeholder="Full name" />
+            <input className="input" placeholder="Email" />
+            <input className="input" placeholder="Confirm email" />
+            <div className="btn" onClick={sendOnClick}>Send</div>
+          </div >
+        )
+      }
+    </div >
   );
 }
 
