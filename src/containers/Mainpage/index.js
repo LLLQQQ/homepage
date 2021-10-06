1b9130afaca5680a88e0df9e7d12f4ab6006f0f4
qiaolei <1170494354@qq.com>
2021-10-06 14:17:25 +0800

    update layout and header content

--- src/containers/Mainpage/index.js
+++ src/containers/Mainpage/index.js
@@ -3,7 +3,9 @@ import style from './index.module.scss';
 function Page() {
   return (
     <div className={style["wrapper"]}>
-      homepage
+      <div className="header">{"Broccoli & Co."}</div>
+      <div className="content"></div>
+      <div className="footer">{"Broccoli & Co."}</div>
     </div>
   );
 }
